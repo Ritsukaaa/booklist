c44300746ba1965bb8479d9a1d11a9b4cdd4eeeb
Ritsukaaa <jhuyfgccc@gmail.com>
2025-05-25 21:37:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ books.forEach(book => {
 </div>
     <div class="stars">${"★".repeat(book.rating)}${"☆".repeat(5 - book.rating)}</div>
     <div class="tags">${book.tags.map(tag => `<span class="label label-a">${tag}</span>`).join("")}</div>
-    <div class="comment"><div class="comment-bar"><p>${book.comment}</p></div></div>
+    <div class="comment"><span class="comment-bar"><p>${book.comment}</p></span></div>
     <img class="watermark" src="images/${book.watermark}"></div>
   `;
 
