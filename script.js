ef82a7516db26921d2d345bf3553c7fe1a8cd9cb
Ritsukaaa <jhuyfgccc@gmail.com>
2025-05-25 21:10:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ books.forEach(book => {
   card.innerHTML = `
   <div class="title"><a href="${book.link}" target="_blank">${book.title}</a><span class="author">作者：${book.author}</span>
     <div class="stars">${"★".repeat(book.rating)}${"☆".repeat(5 - book.rating)}</div>
-    <div class="mata>${book.meta}
+    <div class="mata>${book.meta}</div>
     <div class="tags">${book.tags.map(tag => `<span class="label label-a">${tag}</span>`).join("")}</div>
     <div class="comment"><span class="highlight-block"></span>${book.comment}</div>
     <img class="watermark" src="images/${book.watermark}">
