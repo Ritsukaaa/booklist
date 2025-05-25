fb1299946bc599b3644d465ae1d52f5faba1f828
Ritsukaaa <jhuyfgccc@gmail.com>
2025-05-25 20:33:27 +0800

    Update data.js

--- data.js
+++ data.js
@@ -2,19 +2,10 @@ const books = [
   {
     title: "《養鬼》",
     author: "某某",
-    rating: 4,
+    rating: 5,
     tags: ["現代", "穿書"],
     isDropped: false,
-    comment: "很有趣的故事",
+    comment: "劇情緊湊，角色有魅力",
     watermark: "catgirl.png"
-  },
-  {
-    title: "《黑貓棄文》",
-    author: "某某",
-    rating: 1,
-    tags: ["現代／穿書"],
-    isDropped: true,
-    comment: "不合胃口",
-    watermark: "blackcat.png"
   }
 ];
