e52246dafd5c12ee291046fea27c26bafa9cf0c1
Ritsukaaa <jhuyfgccc@gmail.com>
2025-05-25 20:32:20 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const container = document.getElementById("book-list");
+
+books.forEach(book => {
+  const card = document.createElement("div");
+  card.className = "book-card";
+
+  card.innerHTML = `
+    <div class="book-title">${book.title}<span class="author">${book.author}</span></div>
+    <div class="stars">${"★".repeat(book.rating)}${"☆".repeat(5 - book.rating)}</div>
+    <div class="tags">${book.tags.map(tag => `<span class="label label-a">${tag}</span>`).join("")}</div>
+    <div class="comment"><span class="highlight-block"></span>${book.comment}</div>
+    <img class="watermark" src="images/${book.watermark}">
+  `;
+
+  container.appendChild(card);
+});
