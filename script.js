9c98f834c64168bbe40fe826098a909e43ca18d2
Ritsukaaa <jhuyfgccc@gmail.com>
2025-05-25 21:19:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const container = document.getElementById("book-list");
 
 books.forEach(book => {
   const card = document.createElement("div");
-  card.className = "book-card";
+  card.className = "card";
 
   card.innerHTML = `
-    <div class="book-title">${book.title}<span class="author">${book.author}</span></div>
+    <div class="title">${book.title}<span class="author">${book.author}</span></div>
     <div class="stars">${"★".repeat(book.rating)}${"☆".repeat(5 - book.rating)}</div>
     <div class="tags">${book.tags.map(tag => `<span class="label label-a">${tag}</span>`).join("")}</div>
-    <div class="comment"><span class="highlight-block"></span>${book.comment}</div>
+    <div class="comment"><span class="comment-bar"></span>${book.comment}</div>
     <img class="watermark" src="images/${book.watermark}">
   `;
 
