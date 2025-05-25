64d40724305678071f197fb2f301691d7f7cf580
Ritsukaaa <jhuyfgccc@gmail.com>
2025-05-25 21:15:10 +0800

    Update data.js

--- data.js
+++ data.js
@@ -4,9 +4,8 @@ const books = [
     author: "某某",
     rating: 5,
     tags: ["現代", "穿書"],
-    meta："字數｜攻名字 × 受名字｜口味"
     isDropped: false,
     comment: "劇情緊湊，角色有魅力",
-    watermark: "https://blog-imgs-162.fc2.com/p/o/m/pomelo1122/202505091239326a3.png"
+    watermark: "catgirl.png"
   }
 ];
