b63593a4d236faa07da11ba64e75a74bcec0dd65
Ritsukaaa <jhuyfgccc@gmail.com>
2025-06-05 11:25:24 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,44 @@
+const bookData = [
+{
+ "title": "《他們都說我遇到了鬼》", 
+"author": "青色羽翼",
+ "link": "https://pomelo1122.blog.fc2.com/blog-entry-533.html",
+ "stars": "★☆☆☆☆",
+ "meta": "27%棄｜寧天策（小寧） × 沈建國",
+ "tags": [],
+ "plainTags": [],
+ "comment": "作者文筆明顯駕馭不了第一人稱，gay受設定也是一大敗筆。",
+ "tagA": [],
+ "tagB": [],
+ "tagC": [],
+ "tagD": [],
+ "tagE": [],
+ "tagF": [],
+ "tagG": [],
+ "tagH": [],
+ "tagI": [],
+ "tagDrop": ["現代","靈異","主受第一人稱","雙潔"],
+ "watermark": "https://blog-imgs-162.fc2.com/p/o/m/pomelo1122/202505091239326a3.png", "watermarkWidth": "90px"
+},
+{"title": "《槐鬼之家有惡夫》", "author": "鏨刀爺們", "link": "https://pomelo1122.blog.fc2.com/blog-entry-2.html", "stars": "★★★☆☆", "meta": "22萬字｜槐鬼（張赫） × 賀君｜香豔肥肉", "tags": [], "plainTags": [], "comment": "攻受人設是我的天菜，雖然番外有點狗尾續貂但瑕不掩瑜。", "tagA": ["現代"], "tagB": [], "tagC": ["懸疑","靈異"], "tagD": ["第三人稱"], "tagE": [], "tagF": ["年下","鬼攻"], "tagG": [], "tagH": ["強強"], "tagI": ["HE"], "tagDrop": [], "watermark": "https://blog-imgs-162.fc2.com/p/o/m/pomelo1122/202505091239366f3.png", "watermarkWidth": "120px"}, 
+{
+ "title": "《養鬼》",
+ "author": "鏨刀爺們",
+ "link": "https://pomelo1122.blog.fc2.com/blog-entry-1.html",
+ "stars": "★☆☆☆☆",
+ "meta": "四刷100%棄｜沈誠 × 沈夏冬｜有肉",
+ "tags": [],
+ "plainTags": [],
+ "comment": "作者急著完結，導致受人設反復橫跳，最終爛尾。",
+ "tagA": [], "tagB": [], "tagC": [], "tagD": [], "tagE": [], "tagF": [], "tagG": [], "tagH": [], "tagI": [],
+ "tagDrop": [
+"現代",
+"懸疑",
+"父子",
+"年下",
+"美攻",
+"NE"
+],
+ "watermark": "https://blog-imgs-162.fc2.com/p/o/m/pomelo1122/202505091239326a3.png", "watermarkWidth": "90px"
+}
+];
